16d9dbd650c8c862da3b2588c8afe0797bd06fd5
fqhd <fahd31256@gmail.com>
2021-12-08 18:33:01 +0100

    ES6 style imports

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-require('dotenv').config();
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+import * as DotEnv from 'dotenv';
+DotEnv.config();
+import { Client, Intents } from 'discord.js';
+const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', async message => {
-	if(message.content == 'what am i'){
-		message.channel.send('You are beautiful ;)');
+	switch(message.content){
+		case 'what am i':
+			message.channel.send('You are beautiful ;)');
+		break;
+		case 'gimme cutie':
+			// const response = await fetch(`https://g.tenor.com/v1/registershare?id=8776030&key=${process.env.TENOR_KEY}&q=cutie`);
+			// const json = await response.json();
+			// console.log(json);
+		break;
+		case 'bot stop':
+			console.log('Stopping bot...');
+			await message.channel.send('Stopping bot...');
+			client.destroy();
+		break;
 	}
 });
-
 client.login(process.env.TOKEN);
