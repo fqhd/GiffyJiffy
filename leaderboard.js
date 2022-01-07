d4aa4e2c7f5f38bc0d90f721f064694c1de43588
fqhd <fahd31256@gmail.com>
2022-01-07 23:34:55 +0100

    update

--- leaderboard.js
+++ leaderboard.js
@@ -1,21 +1,28 @@
 import fetch from 'node-fetch';
+let is_active = false;
 
 export async function update_leaderboard(tokens, message, client){
+	if(!is_active){
+		is_active = true;
+	}else{
+		message.channel.send('Leaderboard is already active');
+		return;
+	}
+
 	const names = ["Fahd", "Amine", "Omar", "Mehdi", "Samar", "Yahia", "Sarah", "Axed", "Imane", "Jihwan"];
 	const usernames = ["PANV_xgn3lYQXsNSdCXViBhtIlMvzjvJnku-jdLHmCuFj0mF", "hDrk5vOdOW9AoTzs6WOvlb5Orctn_dMtsfgvzYPqxdtsJJmL", "o60Asiz2h1YejtyMF1SIg-QAz0RQxvNltcmGmRZcbMfrtDQW", "FoEoLD2s3QuHi7iggV_oSGxelhdgi7lzj-dgiIOKfamk0lBQ", "EXpLqDc2TG9hYALDdbPzBkVsr7y54UJPMeoahW7x3H6Bms1C", "fIrlYQ9O0dE9uAtpRRMBx2Z66bhE82FAhgGvjjx-ZxQGYihW", "i_WpCTFvx1feyR29DcD13Fl2b2bvffLpb8bI1-eurOLxLFiC", "5ZxZUR3iPXMjbhjlNSY2d5ixKYYtFJ19Q89YDafTyIPQ0sJK", "Uszb-967hvrhwyGzGe_tnsbh6STIDGn-Bi-X1dm_DfaRROE8", "Dw5v-dla666TOJ8PX8oRGJ9nEOWaaopjsb9eO4_H8fM893Vu"];
 
 	const leaderboardArray = [];
-	let leaderboardString = "Congratulations to everyone for ranking up. Summer just started, we all got no life so I don't expect anyone to be travelin... That said, Goodluck and Have fun on the rift!! :)\n\n";
+	let leaderboardString = "Wadup laaadz, fucked up season, but it's fiiine we shilliiiiiin(if you're not on the leaderboard it's cuz u ain't ranked yet l0ser)\n\n";
 
 	// Adding ranks to leaderboard
 	for(let i = 0; i < names.length; i++){
-		const player = await getRank(usernames[i]).catch(err => {
-			console.log(err);
-			return;
-		});
-		const mmr = rankToMMR(player.tier, player.rank, player.lp);
-		const name = names[i];
-		leaderboardArray.push({ name, player, mmr });
+		const player = await getRank(usernames[i]);
+		if(player){
+			const mmr = rankToMMR(player.tier, player.rank, player.lp);
+			const name = names[i];
+			leaderboardArray.push({ name, player, mmr });
+		}
 	}
 
 	// Sorting the array
@@ -30,12 +37,14 @@ export async function update_leaderboard(tokens, message, client){
 	// Adding date and time to leaderboard
 	leaderboardString += "\n";
 
-	leaderboardString += "Last Updated: " + Date();
+	leaderboardString += "Next Updated: " + new Date(new Date().getTime() + 5*60*1000);
 
 	// Getting channel, leaderboard message, and leaderboard log message
 	const channel = await client.channels.fetch("831148754181816351");
 	const leaderboard_message = await channel.messages.fetch('925875313680990279');
 	leaderboard_message.edit(leaderboardString);
+
+	setTimeout(update_leaderboard, 1000 * 60 * 5, tokens, message, client); // Call this function in 5 minutes
 }
 
 function compare(a, b){
@@ -96,12 +105,8 @@ function rankToMMR(tier, rank, lp){
 async function getRank(puuid){
 	let region = (puuid == "Uszb-967hvrhwyGzGe_tnsbh6STIDGn-Bi-X1dm_DfaRROE8") ? "na1" : "euw1";
 
-	try {
-		var response = await fetch("https://" + region + ".api.riotgames.com/lol/league/v4/entries/by-summoner/" + puuid + "?api_key=" + process.env.RIOT_KEY);
-		var data = await response.json();
-	} catch(err){
-		console.log(err);
-	}
+	var response = await fetch("https://" + region + ".api.riotgames.com/lol/league/v4/entries/by-summoner/" + puuid + "?api_key=" + process.env.RIOT_KEY).catch(err => console.log(err));
+	var data = await response.json().catch(err => console.log(err));
 	if(response.status != 200){
 		console.log("Reponse failed with status code: " + response.status);
 		return null;
