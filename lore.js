921d774be9bf0c4f47bd6b6032ea3d94c596a55f
fqhd <fahd31256@gmail.com>
2022-06-29 16:08:28 +0100

    style: prettier

--- lore.js
+++ lore.js
@@ -1,15 +1,23 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-export function lore(tokens, message, client){
-	const champ = tokens[0];
-	if(tokens[0] == "Fahd"){
-		message.channel.send("Fahd is a one of the mose sacred NPCs of league of legends. Notorious for his inting skills and dogshit mechanics, he is able consistently tilt amine regardless of what he does in the game, if he split pushes, amine rages why he didn't tp, and when he tps, amine flames his tp. This never ending cycle of toxicicity continues between these individuals who will never make it past plat 3.");
-	}else{
-		fetch(`http://ddragon.leagueoflegends.com/cdn/12.12.1/data/en_US/champion/${champ}.json`).then(response => response.json()).then(json => {
-		message.channel.send(json.data[champ].lore);
-	}).catch(err => {
-		console.log(err);
-			message.channel.send("Can't find champion, try different spelling perhaps?");
-		});
-	}
+export function lore(tokens, message, client) {
+  const champ = tokens[0];
+  if (tokens[0] == "Fahd") {
+    message.channel.send(
+      "Fahd is a one of the mose sacred NPCs of league of legends. Notorious for his inting skills and dogshit mechanics, he is able consistently tilt amine regardless of what he does in the game, if he split pushes, amine rages why he didn't tp, and when he tps, amine flames his tp. This never ending cycle of toxicicity continues between these individuals who will never make it past plat 3."
+    );
+  } else {
+    fetch(
+      `http://ddragon.leagueoflegends.com/cdn/12.12.1/data/en_US/champion/${champ}.json`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        message.channel.send(json.data[champ].lore);
+      })
+      .catch((err) => {
+        message.channel.send(
+          "Can't find champion, try different spelling perhaps?"
+        );
+      });
+  }
 }
