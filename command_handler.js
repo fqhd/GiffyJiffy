cbcc14fafc6daf64c3208839a939088ddad7ec97
fqhd <fahd31256@gmail.com>
2022-07-01 17:36:23 +0100

    fix: typo

--- command_handler.js
+++ command_handler.js
@@ -3,7 +3,7 @@ import { stop } from "./stop.js";
 import { asciify } from "./asciify.js";
 import { update_leaderboard } from "./leaderboard.js";
 import { lore } from "./lore.js";
-import openAICommands from "./openAICommands";
+import openAICommands from "./openAICommands.js";
 
 export const commands_map = {
   gimme,
